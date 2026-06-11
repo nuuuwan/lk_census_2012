7e8e192d041dba99c263e71a306cb053d677baf4
nuuuwan <nuuuwan@gmail.com>
2026-06-11 18:29:57 +0530

    Add handling for 'etc' in parse_int method to improve integer parsing robustness

--- src/census/pdf_source_file/ParseUtils.py
+++ src/census/pdf_source_file/ParseUtils.py
@@ -4,6 +4,7 @@ class ParseUtils:
     def parse_int(x):
         x = str(x)
         x = x.replace(",", "")
+        x = x.replace("etc", "")
         if x == "" or x == "-":
             return 0
         x = x.replace(" ", "")
